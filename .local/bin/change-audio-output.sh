5e48c62b241cb92ce657abc0ea129baff26b2e52
Julio Arias <jarias@users.noreply.github.com>
2024-12-05 11:35:45 -0600

    chore: fix change-audio-output script

--- .local/bin/change-audio-output.sh
+++ .local/bin/change-audio-output.sh
@@ -10,7 +10,7 @@ echo "Current sink id $current_sink_id"
 headset_sink_id=$(wpctl status | grep "G535 Wireless Gaming Headset Analog Stereo" | sed -E 's/│//g' | awk '{$1=$1};1' | cut -d' ' -f1 | sed -E 's/\.//g')
 echo "Headset sink id $headset_sink_id"
 
-speakers_sink_id=$(wpctl status | grep "Family 17h/19h HD Audio Controller Analog Stereo" | head -n 1 | sed -E 's/│\s+.\s+//g' | awk '{$1=$1};1' | cut -d' ' -f1 | sed -E 's/\.//g')
+speakers_sink_id=$(wpctl status | grep "Family 17h/19h/1ah HD Audio Controller Analog Stereo" | head -n 1 | sed -E 's/│\s+.\s+//g' | awk '{$1=$1};1' | cut -d' ' -f1 | sed -E 's/\.//g')
 echo "Speakers sink id $speakers_sink_id"
 
 if [[ $current_sink_id == "$speakers_sink_id" ]]; then
