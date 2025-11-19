e336f7254caf9b997c07e768b9cde773de2d6f26
Julio Arias <jarias@users.noreply.github.com>
2025-11-18 23:42:32 -0600

    lsp mappings

--- .config/nvim/lua/config/mappings.lua
+++ .config/nvim/lua/config/mappings.lua
@@ -13,8 +13,21 @@ vim.keymap.set("n", "<A-.>", "<Cmd>BufferNext<CR>", { desc = "Previous Buffer", 
 vim.keymap.set("n", "<A-c>", "<Cmd>BufferClose<CR>", { desc = "Close Buffer", noremap = true, silent = true })
 
 vim.keymap.set(
-	"n",
-	"<leader>ed",
-	"<Cmd>lua MiniFiles.open()<CR>",
-	{ desc = "Open Mini Files", noremap = true, silent = true }
+  "n",
+  "<leader>ed",
+  "<Cmd>lua MiniFiles.open()<CR>",
+  { desc = "Open Mini Files", noremap = true, silent = true }
 )
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(event)
+    local bufmap = function(mode, rhs, lhs)
+      vim.keymap.set(mode, rhs, lhs, { buffer = event.buf })
+    end
+
+    -- These are custom keymaps
+    bufmap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>")
+    bufmap("n", "grt", "<cmd>lua vim.lsp.buf.type_definition()<cr>")
+    bufmap("n", "grd", "<cmd>lua vim.lsp.buf.declaration()<cr>")
+  end,
+})
